fd5895006ff1cd9713e7037e8292014ae4530672
David Crossman <22548398+DavidCrossman@users.noreply.github.com>
2024-08-17 21:05:37 +0100

    Added substitutions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use chumsky::Parser;
+use model::*;
+use substitution::*;
 
 mod lexer;
+mod model;
 mod parser;
+mod substitution;
 
 fn main() {
     let result = lexer::lexer().parse("let x = y (z 5) in λp r -> r p x");
@@ -14,4 +18,36 @@ fn main() {
             Err(e) => println!("{e:?}"),
         }
     }
+
+    let mut test = PolyType::Quantifier(
+        "x".to_owned(),
+        Box::new(PolyType::Mono(MonoType::Con(TypeConstructor::Function(
+            Box::new(MonoType::Var("x".to_owned())),
+            Box::new(MonoType::Con(TypeConstructor::Function(
+                Box::new(MonoType::Var("y".to_owned())),
+                Box::new(MonoType::Var("z".to_owned())),
+            ))),
+        )))),
+    );
+
+    let mut sub = Substitution::new();
+    sub.insert("x".to_owned(), MonoType::Var("e".to_owned()));
+    sub.insert(
+        "y".to_owned(),
+        MonoType::Con(TypeConstructor::Function(
+            Box::new(MonoType::Var("a".to_owned())),
+            Box::new(MonoType::Var("b".to_owned())),
+        )),
+    );
+
+    let mut sub2 = Substitution::new();
+    sub2.insert("a".to_owned(), MonoType::Var("c".to_owned()));
+
+    sub.combine(&sub2);
+
+    println!("{test}");
+
+    test.substitute_mut(&sub);
+
+    println!("{test}");
 }
---
+++ src/substitution.rs
@@ -0,0 +1,72 @@
+use crate::model::*;
+use derive_more::derive::{Deref, DerefMut};
+use std::collections::HashMap;
+
+#[derive(Default, PartialEq, Eq, Deref, DerefMut, Clone)]
+pub struct Substitution(HashMap<String, MonoType>);
+
+impl IntoIterator for Substitution {
+    type Item = (String, MonoType);
+
+    type IntoIter = <HashMap<String, MonoType> as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+impl Substitution {
+    pub fn new() -> Self {
+        Self(HashMap::new())
+    }
+
+    pub fn combine(&mut self, other: &Self) {
+        self.values_mut().for_each(|m| m.substitute_mut(other));
+        self.extend(other.clone());
+    }
+}
+
+pub trait Substitute {
+    fn substitute_mut(&mut self, subst: &Substitution);
+
+    fn substitute(mut self, subst: &Substitution) -> Self
+    where
+        Self: Sized,
+    {
+        self.substitute_mut(subst);
+        self
+    }
+}
+
+impl Substitute for MonoType {
+    fn substitute_mut(&mut self, subst: &Substitution) {
+        match self {
+            MonoType::Var(s) if subst.contains_key(s) => *self = subst[s].clone(),
+            MonoType::Con(TypeConstructor::List(m)) => m.substitute_mut(subst),
+            MonoType::Con(TypeConstructor::Function(m1, m2)) => {
+                m1.substitute_mut(subst);
+                m2.substitute_mut(subst);
+            }
+            _ => {}
+        }
+    }
+}
+
+impl Substitute for PolyType {
+    fn substitute_mut(&mut self, subst: &Substitution) {
+        match self {
+            PolyType::Mono(m) => m.substitute_mut(subst),
+            PolyType::Quantifier(s, p) => {
+                let mut subst = subst.clone();
+                subst.remove(s);
+                p.substitute_mut(&subst);
+            }
+        }
+    }
+}
+
+impl Substitute for Context {
+    fn substitute_mut(&mut self, subst: &Substitution) {
+        self.values_mut().for_each(|m| m.substitute_mut(subst));
+    }
+}
