3cb489cd34c667ae8361ca7b6feae1a845d613cd
David Crossman <22548398+DavidCrossman@users.noreply.github.com>
2024-08-21 22:47:51 +0100

    Added algorithm M

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -27,24 +27,69 @@ pub fn w(
             let (s2, m2, n) = w(&context.clone().substitute(&s1), e2, n)?;
             let beta = MonoType::Var(TypeVariable::Inferred(n));
             let s3 = unify(
-                m1.substitute(&s2),
                 TypeConstructor::Function(Box::new(m2), Box::new(beta.clone())).into(),
+                m1.substitute(&s2),
             )?;
             Ok((s1.combine(&s2.combine(&s3)), beta.substitute(&s3), n + 1))
         }
         Expression::Abs(x, e) => {
             let beta = MonoType::Var(TypeVariable::Inferred(n));
-            let context = context.clone() + (x.clone(), beta.clone());
-            let (s, m, n) = w(&context, e, n + 1)?;
+            let (s, m, n) = w(&(context.clone() + (x.clone(), beta.clone())), e, n + 1)?;
             let m = MonoType::Con(TypeConstructor::Function(Box::new(beta), Box::new(m)));
             Ok((s.clone(), m.substitute(&s), n))
         }
         Expression::Let(x, e1, e2) => {
             let (s1, m1, n) = w(context, e1, n)?;
-            let p = m1.generalise(&context.clone().substitute(&s1));
-            let context = context.clone() + (x.clone(), p);
-            let (s2, m2, n) = w(&context.substitute(&s1), e2, n)?;
+            let context = context.clone().substitute(&s1);
+            let p = m1.generalise(&context);
+            let (s2, m2, n) = w(&(context + (x.clone(), p)), e2, n)?;
             Ok((s1.combine(&s2), m2, n))
+        }
+    }
+}
+
+pub fn m(
+    context: &Context,
+    expr: &Expression,
+    t: MonoType,
+    n: usize,
+) -> Result<(Substitution, usize), TypeError> {
+    match expr {
+        Expression::Lit(lit) => unify(
+            t,
+            MonoType::Con(match lit {
+                Literal::Bool(_) => TypeConstructor::Bool,
+                Literal::Nat(_) => TypeConstructor::Int,
+            }),
+        )
+        .map(|s| (s, n)),
+        Expression::Var(x) => match context.env.get(x) {
+            Some(p) => unify(t, p.clone().instantiate(n)).map(|s| (s, n + p.quantifiers.len())),
+            None => Err(TypeError::UnknownVariable(x.clone())),
+        },
+        Expression::App(e1, e2) => {
+            let beta = MonoType::Var(TypeVariable::Inferred(n));
+            let t = TypeConstructor::Function(Box::new(beta.clone()), Box::new(t));
+            let (s1, n) = m(context, e1, t.into(), n + 1)?;
+            let (s2, n) = m(&context.clone().substitute(&s1), e2, beta.substitute(&s1), n)?;
+            Ok((s1.combine(&s2), n))
+        }
+        Expression::Abs(x, e) => {
+            let beta1 = MonoType::Var(TypeVariable::Inferred(n));
+            let beta2 = MonoType::Var(TypeVariable::Inferred(n + 1));
+            let t2 = TypeConstructor::Function(Box::new(beta1.clone()), Box::new(beta2.clone())).into();
+            let s1 = unify(t, t2)?;
+            let context = context.clone().substitute(&s1) + (x.clone(), beta1.substitute(&s1));
+            let (s2, n) = m(&context, e, beta2.substitute(&s1), n + 2)?;
+            Ok((s1.combine(&s2), n))
+        }
+        Expression::Let(x, e1, e2) => {
+            let beta = MonoType::Var(TypeVariable::Inferred(n));
+            let (s1, n) = m(context, e1, beta.clone(), n + 1)?;
+            let context = context.clone().substitute(&s1);
+            let p = beta.substitute(&s1).generalise(&context);
+            let (s2, n) = m(&(context + (x.clone(), p)), e2, t.substitute(&s1), n)?;
+            Ok((s1.combine(&s2), n))
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use chumsky::Parser;
 use model::*;
+use substitution::Substitute;
 
 mod algorithm;
 mod lexer;
@@ -12,10 +13,20 @@ mod variable;
 fn main() {
     match lexer::lexer().parse("let id = λx -> x in (λa b -> a) (id true) (id 0)") {
         Ok(tokens) => match parser::parser().parse(tokens) {
-            Ok(expr) => match algorithm::w(&Context::new(), &expr, 0) {
-                Ok((s, m, _)) => println!("type checking {expr} succeeded\ntype: {m}\nsubst:{s}"),
-                Err(e) => println!("type checking {expr} failed\nerror: {e}"),
-            },
+            Ok(expr) => {
+                println!("type checking expression: {expr}");
+                println!("algorithm w");
+                match algorithm::w(&Context::new(), &expr, 0) {
+                    Ok((_, m, _)) => println!("type: {m}"),
+                    Err(e) => println!("error: {e}"),
+                }
+                println!("algorithm m");
+                let t = MonoType::Var(TypeVariable::UserDefined("a".to_owned()));
+                match algorithm::m(&Context::new(), &expr, t.clone(), 0) {
+                    Ok((s, _)) => println!("type: {}", t.substitute(&s)),
+                    Err(e) => println!("error: {e}"),
+                }
+            }
             Err(e) => println!("parse error: {e:?}"),
         },
         Err(e) => println!("lexical error: {e:?}"),
